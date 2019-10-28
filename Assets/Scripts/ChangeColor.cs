893e3d2302ef71bd9e864b80d190965088e7a5f5
nenoi <daisuke-n@digirea.com>
2019-10-28 16:34:10 +0900

    First commit.

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+// SpriteRendererでも可
+[RequireComponent(typeof(Image))]
+public class ChangeColor : MonoBehaviour {
+    public float Time = 0.2f;
+
+    private Image selfImage;
+
+    private void Awake() {
+        selfImage = GetComponent<Image>();
+    }
+
+    void Update() {
+        if (Music.IsPlaying && Music.IsJustChangedBar()) {
+            Color color = new Color(
+                Random.Range(0.5f, 0.8f),
+                Random.Range(0.5f, 0.8f),
+                Random.Range(0.5f, 0.8f),
+                1f
+            );
+            selfImage.DOColor(color, Time);
+        }
+    }
+}
---
+++ Assets/Scripts/SEPlayQuantizeButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(AudioSource))]
+public class SEPlayQuantizeButton : MonoBehaviour, IPointerDownHandler {
+    public void OnPointerDown(PointerEventData eventData) {
+        Music.QuantizePlay(this.GetComponent<AudioSource>());
+    }
+}
